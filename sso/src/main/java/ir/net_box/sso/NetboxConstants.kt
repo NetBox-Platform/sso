dbdc346e37a62e279018e86ecaebb7520eabbb0a
medise <medise08@gmail.com>
2023-12-05 12:11:41 +0100

    Add NetboxConstants file

---
+++ sso/src/main/java/ir/net_box/sso/NetboxConstants.kt
@@ -0,0 +1,5 @@
+package ir.net_box.sso
+
+const val LAUNCHER_PACKAGE_NAME = "ir.net_box.launcher"
+const val NET_STORE_PACKAGE_NAME = "ir.net_box.store"
+const val MINIMUM_SUPPORT_LAUNCHER = "4.4.0"
