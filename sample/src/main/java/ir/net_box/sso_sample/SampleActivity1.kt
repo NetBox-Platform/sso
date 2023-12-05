2663c7e94588e5462f6c2b2561c814f3cac188b2
medise <medise08@gmail.com>
2023-12-05 10:19:53 +0100

    setup project

---
+++ sample/src/main/java/ir/net_box/sso_sample/SampleActivity1.kt
@@ -0,0 +1,11 @@
+package ir.net_box.sso_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SampleActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sample_layout)
+    }
+}
